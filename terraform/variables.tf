b2e68723f5b984816c189f7efa80d80da7ebaabc
Harini Murugesan <harini.murugesan30@gmail.com>
2025-02-27 02:22:27 +0100

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "kaggle_username" {
+  description = "harinimurugesan30"
+  type        = string
+}
+
+variable "kaggle_key" {
+  description = "afc68f1bdad805ad1bffd88c5ebf3e0c"
+  type        = string
+}
